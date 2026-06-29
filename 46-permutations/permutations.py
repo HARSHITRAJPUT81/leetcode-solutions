a1f3bc37ef4142a53ef1c06f38cc5ad76d26fe34
Harshit Rajput <154085497+HARSHITRAJPUT81@users.noreply.github.com>
2026-06-29 22:27:14 +0530

    Time: 1 ms (45.25%) | Memory: 19.5 MB (65.32%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,23 @@
+class Solution:
+    def permute(self, nums):
+        ans = []
+        used = [False] * len(nums)
+
+        def backtrack(path):
+            if len(path) == len(nums):
+                ans.append(path[:])
+                return
+
+            for i in range(len(nums)):
+                if not used[i]:
+                    used[i] = True
+                    path.append(nums[i])
+
+                    backtrack(path)
+
+                    path.pop()
+                    used[i] = False
+
+        backtrack([])
+        return ans
+
