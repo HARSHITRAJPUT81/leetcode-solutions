b1b03676a4e42d597f859c7a4dd4953b57c562a0
Harshit Rajput <154085497+HARSHITRAJPUT81@users.noreply.github.com>
2026-07-12 23:29:18 +0530

    Time: 51 ms (49.80%) | Memory: 19.7 MB (86.50%) - LeetSync

---
+++ 133-clone-graph/clone-graph.py
@@ -0,0 +1,31 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=0, neighbors=None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node):
+        if not node:
+            return None
+
+        visited = {}
+
+        def dfs(curr):
+            # Already cloned
+            if curr in visited:
+                return visited[curr]
+
+            # Create clone
+            clone = Node(curr.val)
+            visited[curr] = clone
+
+            # Clone neighbors
+            for neighbor in curr.neighbors:
+                clone.neighbors.append(dfs(neighbor))
+
+            return clone
+
+        return dfs(node)
