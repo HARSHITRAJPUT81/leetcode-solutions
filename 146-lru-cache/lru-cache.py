ccfa3a4957364b8b673e96a65de79204ab87702a
Harshit Rajput <154085497+HARSHITRAJPUT81@users.noreply.github.com>
2026-07-13 23:59:17 +0530

    Time: 152 ms (21.23%) | Memory: 78.7 MB (16.35%) - LeetSync

---
+++ 146-lru-cache/lru-cache.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__(self, key=0, value=0):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {}
+
+        self.head = Node()  # Dummy head
+        self.tail = Node()  # Dummy tail
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _remove(self, node):
+        prev = node.prev
+        nxt = node.next
+        prev.next = nxt
+        nxt.prev = prev
+
+    def _insert(self, node):
+        node.next = self.head.next
+        node.prev = self.head
+        self.head.next.prev = node
+        self.head.next = node
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+
+        node = self.cache[key]
+        self._remove(node)
+        self._insert(node)
+        return node.value
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self._remove(self.cache[key])
+
+        node = Node(key, value)
+        self.cache[key] = node
+        self._insert(node)
+
+        if len(self.cache) > self.cap:
+            lru = self.tail.prev
+            self._remove(lru)
+            del self.cache[lru.key]
